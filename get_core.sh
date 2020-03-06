04d9f7cbfd2feb85cba269b41f7915bf3c685631
shw1n <ashwinmathews@gmail.com>
2020-03-05 18:12:48 -0800

    Remove grive b/c google's api update broke unverified open source apps

--- get_core.sh
+++ get_core.sh
@@ -15,7 +15,6 @@ prompt() {
     fi
 }
 
-
 install_tmux_ag() {
 sudo apt install tmux silversearcher-ag 
 }
@@ -39,51 +38,6 @@ install_evernote() {
     wget "https://archive.org/download/Evernote5.8.13.8152/Evernote_5.8.13.8152.exe" -O /tmp/evernote.exe
     chmod +x /tmp/evernote.exe
     wine /tmp/evernote.exe
-}
-
-# grive
-
-# Shouldn't have to do this again (client keys are already in script), but just in case:
-
-# Google recently blocked grive's API's for general use, so to make it work
-# you must use your own API keys instead of vitalif's 
-
-#1 - Go to https://console.developers.google.com/apis/library. Login with your Google account and create a new project on Google Cloud Platform. You can also use an existing one.
-#2 - Search for Google Drive API and enable it.
-#3 - Go to API Credentials page (https://console.developers.google.com/apis/credentials)
-#3 - Create a new OAuth Client credential. You will be provided with a id and a secret. Save both because you gonna need it for grive2.
-#Then you need to follow this repo README, download grive2, build and install. Then run
-#
-#grive -a --id [client id] --secret [client secret]
-#
-#On your Drive folder and it will probably work.
-#If you want to use your ID and secret to build grive2 instead of using above command, you can edit the keys in
-#
-#grive2/grive/src/main.cc
-#
-#Lines 49 to 50 in 3929909
-# const std::string default_id            = "615557989097-i93d4d1ojpen0m0dso18ldr6orjkidgf.apps.googleusercontent.com" ; 
-# const std::string default_secret        = "xiM8Apu_WuRRdheNelJcNtOD" ; 
-
-install_grive() {
-    mkdir $HOME/google-drive
-
-    # This .griveignore only loads keepass by default
-    cp griveignore $HOME/google-drive/.griveignore
-
-    # Build grive
-    sudo apt-get install git cmake build-essential libgcrypt20-dev libyajl-dev \
-            libboost-all-dev libcurl4-openssl-dev libexpat1-dev libcppunit-dev binutils-dev \
-                debhelper zlib1g-dev dpkg-dev pkg-config
-    git clone https://github.com/vitalif/grive2.git $HOME/tools/grive2
-    cd $HOME/tools/grive2
-    dpkg-buildpackage --no-sign
-
-    # Install grive
-    cd build
-    sudo make install
-    cd $HOME/google-drive
-    grive -a --id 199239432923-440ikkm4v8n75c3gp0cjju1gblaechk5.apps.googleusercontent.com --secret JftXBH5MiQAZJU6DxeB-M0Vw
 }
 
 # Main
